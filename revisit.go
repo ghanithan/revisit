3baca64e30c4681ac17aa80407e67dd31d5b5101
Ghanithan Subramani <ghanithan.subramani@gmail.com>
2024-10-16 06:53:11 +0530

    leetcode

--- revisit.go
+++ revisit.go
@@ -8,5 +8,18 @@ func Revisit(name string) string {
 	greetings := fmt.Sprintf("hello %s", name)
 	fmt.Println(greetings)
 	return greetings
+}
+
+func Merge(nums1 []int, m int, nums2 []int, n int) {
+	nums1 = append(nums1[:m], nums2...)
+	for i := 0; i < m+n; i++ {
+		for j := i + 1; j < m+n; j++ {
+			if nums1[i] > nums1[j] {
+				swap := nums1[i]
+				nums1[i] = nums1[j]
+				nums1[j] = swap
+			}
+		}
+	}
 
 }
--- revisit_test.go
+++ revisit_test.go
@@ -1,10 +1,25 @@
 package revisit
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestRevisit(t *testing.T) {
 	want := "hello world"
 	if got := Revisit("world"); got != want {
 		t.Errorf("revisit() = %q, want %q", got, want)
+	}
+}
+
+func TestMerge(t *testing.T) {
+	want := []int{1, 2, 2, 3, 5, 6}
+	nums1 := []int{3, 1, 2, 0, 0, 0}
+	m := 3
+	nums2 := []int{2, 5, 6}
+	n := 3
+	Merge(nums1, m, nums2, n)
+	if got := nums1; !reflect.DeepEqual(got, want) {
+		t.Errorf("Merge() = %q, want %q", got, want)
 	}
 }
